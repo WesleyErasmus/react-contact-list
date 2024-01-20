2baf423d9166566394927503fa110be005018b40
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-01-20 10:52:40 +0200

    add/interface, comments, & remove/React.StrictMode
    1. Added a type interface to define the API data structures
    
    2. Added comments to the useFetchData.tsx custom React hook
    
    3. Removed React strict mode from main.tsx as it was causing the useEffect API fetch to render twice

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  /* Removed React.StrictMode as it was causing the useEffect data fetch twice due to component re-rendering. 
+  >>
+  https://github.com/axios/axios/issues/2825#issuecomment-883635938
+  <<
+  */
+  <>
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
--- src/utils/useFetchData.tsx
+++ src/utils/useFetchData.tsx
@@ -4,29 +4,52 @@ import axios from 'axios';
 // React hooks
 import { useEffect, useState } from 'react';
 
-interface Data {
-  // API data response will go here
+// Defining data structure
+interface ContactData {
+  picture: { large: string; thumbnail: string };
+  name: { first: string; last: string };
+  gender: string;
+  dob: { age: number };
+  cell: number;
+  email: string;
+  location: { city: string; state: string; country: string };
 }
 
 const useFetchData = () => {
-  const [data, setData] = useState<Data | null>(null);
+  // React states
+
+  // <ContactData[]>: This type specifies that the data state variable will hold an array of objects that conform to the ContactData interface above.
+  const [data, setData] = useState<ContactData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // Effect triggers once when components initially render as the dependency array is is empty
   useEffect(() => {
+    // Defining the fetchData function
     const fetchData = async () => {
       try {
+        // Asynchronously calling axios to fetch the API data
         const response = await axios.get('https://randomuser.me/api/');
-        setData(response.data);
-        console.log(response);
+        // Setting data state variable with data from API
+        setData(response.data.results);
+        console.log(response.data.results);
+        // Error handling
       } catch (error) {
         console.error(error);
+        /* The "finally" code block guarantees that the below code will always run regardless of the success or fail of "try" and "catch" code blocks. 
+        >>
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+        << */
       } finally {
+        // Set loading to false after the "catch" and "try" code blocks have run.
         setIsLoading(false);
       }
     };
+    // Instantiating the fetchData function
     fetchData();
+    // Defining an empty array as the useEffects second parameter
   }, []);
 
+  // Returning the data and isLoading state which is updated by the fetchData function. (allows other components to consume this data and loading state)
   return { data, isLoading };
 };
 
