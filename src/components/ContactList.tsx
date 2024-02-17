08e5ff61ea4d2278c9a4f38923ad421dc68f91b5
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-02-17 12:54:27 +0200

    added/view-selected-contacts-componet
    1. Added a component which displays contacts that are selected

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -8,11 +8,24 @@ import { Contact } from '../utils/useFetchData';
 import '../styles/contactList.css';
 
 // MUI
-import { Avatar, Box, Card, CardContent, Divider } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Checkbox,
+  Divider,
+} from '@mui/material';
+
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+// import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 
 // COMPONENTS
 import SkeletonContactList from './SkeletonContactList';
 import ContactView from './ContactView';
+import ViewSelectedContacts from './ViewSelectedContacts';
 
 // STATE HOOK
 import { useState } from 'react';
@@ -23,14 +36,36 @@ const ContactList = () => {
   const { data, isLoading } = useFetchData();
 
   // STATE
+  // SINGLE CONTACT VIEW
   const [selectedContact, setSelectedContact] = useState<Contact | null>(null);
+  // SELECTED CONTACTS LIST VIEW
+  const [selectedContacts, setSelectedContacts] = useState<Contact[]>([]);
+
+  /* 1. Select individual contacts
+   2. select all contacts
+   3. Unselect / clear all selected items
+*/
+
+  const handleLogSelectedContacts = () => {
+    console.log('Selected contacts:', selectedContacts);
+  };
+
+  const handleContactSelection = (contact: Contact) => {
+    setSelectedContacts((prevSelectedContacts) => {
+      if (prevSelectedContacts.includes(contact)) {
+        return prevSelectedContacts.filter((c) => c !== contact);
+      } else {
+        return [...prevSelectedContacts, contact];
+      }
+    });
+  };
 
   // OPEN DIALOG FUNCTION
   const handleOpenDialog = (contact: Contact) => {
     setSelectedContact(contact);
   };
 
-// CLOSE DIALOG FUNCTION
+  // CLOSE DIALOG FUNCTION
   const handleCloseDialog = () => {
     setSelectedContact(null);
   };
@@ -43,6 +78,15 @@ const ContactList = () => {
         {isLoading ? 'Loading...' : 'Contact List'}
       </h1>
 
+      {/* TOP NAVBAR */}
+      <div className='top-navbar'>
+        
+        <ViewSelectedContacts
+          onClick={handleLogSelectedContacts}
+          selectedContacts={selectedContacts}
+        />
+      </div>
+
       {/* GRID CONTAINER */}
       <div className='contact-list-grid-container'>
         {/* Maps through API data once it has been retrieved, else displays loading message - using ternary operator */}
@@ -53,8 +97,6 @@ const ContactList = () => {
                 <div>
                   {/* CARD */}
                   <Card
-                  // Open dialog on click function
-                    onClick={() => handleOpenDialog(contact)}
                     className='contact-card'
                     sx={{
                       borderRadius: '12px',
@@ -63,57 +105,98 @@ const ContactList = () => {
                       boxShadow: 'none',
                     }}
                   >
-                    {/* CARD BODY */}
-                    <CardContent className='contact-card-body'>
-                      <div>
-                        {/* CONTACT IMAGE */}
-                        <Avatar
-                          className='contact-list-contact-image'
-                          src={contact.picture.large}
-                          sx={{
-                            width: 60,
-                            height: 60,
-                            margin: 'auto',
-                          }}
-                        />
-                      </div>
-                      <div className='card-text-content'>
-                        <Box
-                          className='contact-list-contact-name'
-                          component='h3'
-                          sx={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            letterSpacing: '0.5px',
-                            marginTop: 1,
-                            marginBottom: 0,
-                          }}
-                        >
-                          {/* CONTACT NAME */}
-                          {contact.name.first} {contact.name.last}
-                        </Box>
-                        <Box
-                          className='location-text-container'
-                          component='span'
-                          sx={{
-                            fontSize: 13,
-                            color: 'grey.500',
-                            marginBottom: '0.875em',
-                          }}
-                        >
-                          {/* CONTACT LOCATION */}
-                          {contact.location.country}
-                        </Box>
-                      </div>
-                    </CardContent>
-                    <Divider light />
+                    <div
+                      className={
+                        selectedContacts.includes(contact)
+                          ? 'selected-contact'
+                          : ''
+                      }
+                    >
+                      {/* CARD BODY */}
+                      <CardContent className='contact-card-body'>
+                        <div className='card-body-inner'>
+                          <div>
+                            {/* CONTACT IMAGE */}
+                            <Avatar
+                              // Open dialog on click function
+                              className='contact-list-contact-image'
+                              src={contact.picture.large}
+                              sx={{
+                                width: 84,
+                                height: 84,
+                                margin: 'auto',
+                              }}
+                            />
+                          </div>
+                          <div className='card-text-content'>
+                            <Box
+                              className='contact-list-contact-name'
+                              component='h3'
+                              sx={{
+                                fontSize: 14,
+                                fontWeight: 'bold',
+                                letterSpacing: '0.5px',
+                                marginTop: 1,
+                                marginBottom: 0.15,
+                              }}
+                            >
+                              {/* CONTACT NAME */}
+                              {contact.name.first} {contact.name.last}
+                            </Box>
+                            <Box
+                              component='span'
+                              sx={{
+                                fontSize: 13,
+                                color: 'black',
+                                marginBottom: '0.875em',
+                                textWrap: 'no-wrap',
+                              }}
+                            >
+                              {/* CONTACT LOCATION */}
+                              {contact.location.city},
+                            </Box>
+                            <Box
+                              component='div'
+                              sx={{
+                                fontSize: 13,
+                                color: 'black',
+                                marginBottom: '0.35em',
+                              }}
+                            >
+                              {/* CONTACT LOCATION */}
+                              {contact.location.country}
+                            </Box>
+                            <span
+                              className='view-contact-details-button'
+                              onClick={() => handleOpenDialog(contact)}
+                            >
+                              <AddIcCallIcon />
+                            </span>
+                          </div>
+                        </div>
+                        <div className='select-contact-checkbox'>
+                          <Checkbox
+                            checked={selectedContacts.includes(contact)}
+                            onChange={() => handleContactSelection(contact)}
+                            icon={<RadioButtonUncheckedIcon />}
+                            checkedIcon={<CheckCircleIcon />}
+                          />
+                        </div>
+                      </CardContent>
+                    </div>
+                    {/* <CardActions
+                      className='card-actions'
+                      sx={{ justifyContent: 'flex-end' }}
+                    > */}
+                    {/* </CardActions> */}
+                    <Divider light />{' '}
                   </Card>
                 </div>
               </div>
             );
           })
         ) : (
-          // LOADING SKELETON COMPONENT 
+          // LOADING SKELETON COMPONENT
           <SkeletonContactList />
         )}
       </div>
